c4bacfe1742bcf955fe5c9e281e4f304392f83c3
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-01-30 01:16:02 +0900

    user_id生成

--- usecase/user_usecase.py
+++ usecase/user_usecase.py
@@ -1,4 +1,5 @@
 from domain.user_domain import User
+import uuid
 from domain.interface_user_repository import IUserRepository
 
 class UserUsecase:
@@ -6,7 +7,8 @@ class UserUsecase:
         self.user_repository = user_repository
 
     def create_user(self,user_name):
-        user = User(user_name = user_name)
+        user_id = uuid.uuid4()
+        user = User(user_id = user_id, user_name = user_name)
         self.user_repository.create_user(user)
         return
 
