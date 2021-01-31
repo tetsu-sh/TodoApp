a650927b9bb3c36354296dc798653688f58957fc
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-01-31 14:28:30 +0900

    task　テーブル作成コード追加

--- create_table.py
+++ create_table.py
@@ -1,7 +1,9 @@
 
 from infra.sqlite3.settings import SQLITE3_NAME
 import infra.sqlite3.db as db
-from infra.sqlite3.user_repository import *
+from infra.sqlite3.user_repository import Base as BaseUser
+from infra.sqlite3.task_repository import Base as BaseTask
+
 import os
  
  
@@ -10,5 +12,7 @@ if __name__ == "__main__":
     # if not os.path.isfile(path):
  
     # テーブルを作成する
-    Base.metadata.create_all(db.engine)
+    BaseUser.metadata.create_all(db.engine)
+    BaseTask.metadata.create_all(db.engine)
+
  
