0b289cf9f37f4f656383477ef15f1fd7bc5e8b14
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-02-01 21:50:39 +0900

    もどした

--- create_table.py
+++ create_table.py
@@ -1,17 +1,12 @@
 
-from infra.sqlite3.settings import SQLITE3_NAME
 import infra.sqlite3.db as db
 from infra.sqlite3.user_repository import Base as BaseUser
 from infra.sqlite3.task_repository import Base as BaseTask
 from infra.sqlite3.assign_repository import Base as BaseAssign
 
-import os
- 
- 
+
 if __name__ == "__main__":
-    path = SQLITE3_NAME
-    # if not os.path.isfile(path):
- 
+    
     # テーブルを作成する
     BaseUser.metadata.create_all(db.engine)
     BaseTask.metadata.create_all(db.engine)
