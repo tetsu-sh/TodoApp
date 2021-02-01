61b0092d1bd7b524281ea0828f607285dcef43f9
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-02-01 21:33:10 +0900

    インメモリデータ

--- infra/inmemory/inmemory_user_repository.py
+++ infra/inmemory/inmemory_user_repository.py
@@ -1,15 +1,26 @@
 from domain.interface_user_repository import IUserRepository
 from domain.user_domain import User
+import uuid
 
 class InmemoryUserRepository(IUserRepository):
     def __init__(self):
-        pass
+        self.data = []
 
     def create(self, user:User):
-        pass
+        new_user = {
+            "user_id" :user.user_id,
+            "user_name": user.user_name,
+        }
+        self.data.append(new_user)
+        return
 
     def load(self):
-        pass
+        return self.data
 
-    def find(self):
+    def find(self, user_id):
+        res = [k for k ,v in self.data.items() if v==user_id]
+
+        return res
+
+    def delete(self, user_id):
         pass
