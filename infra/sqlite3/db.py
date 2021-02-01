77b705ce172a286f5d4802a6c3e352808fa31082
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2021-02-01 21:32:55 +0900

    戻した

--- infra/sqlite3/db.py
+++ infra/sqlite3/db.py
@@ -1,11 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import os
 
 Base = declarative_base()
 
-RDB_PATH = os.environ["RDB_PATH"]
+RDB_PATH = 'sqlite:///infra/sqlite3/db.sqlite3'
 ECHO_LOG = True
  
 engine = create_engine(
